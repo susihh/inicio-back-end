e7b8139c941e53e4e182132e7de9b0fa55a64f6a
susihh <susana.tuasco@escola.pr.gov.br>
2026-02-24 07:53:36 -0300

    Add files via upload

---
+++ carro.js
@@ -0,0 +1,7 @@
+carro1 = "A";
+km1 = 150km;
+p1 = 0,60;
+carro2 = "gol" 240km 
+q2 = 240km;
+preço por quilometros = 0,46;
+console.log("o preço do aluguel do", carro1, "é", quilometros rodados * preço por quilometros);

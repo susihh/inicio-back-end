8f6ab5d44b05e508d306a84bd33c6ee4e37d4549
susihh <susana.tuasco@escola.pr.gov.br>
2026-03-17 08:52:30 -0300

    Update filtrar.js

--- filtrar.js
+++ filtrar.js
@@ -1,16 +1,23 @@
-const usarios =[
-    { nome: 'carlos', idade: 32 }, 
-    { nome: 'ana', idade: 28 },
-    { nome: 'felipe', idade: 40 },
+const usuarios = [
+    // criando uma lista (array)
+    { nome:"carlos", idade: 32 }, 
+    { nome:"Ana", idade: 28 },
+    { nome:"felipe", idade: 40 }
 ];
-// encontrando um objeto
-const ana = usuarios.find(usuario => usuario.nome === 'ana');
-console.log(ana);
+// encontrando um objeto-find-===
+const Ana = usuarios.find(usuarios => usuarios.nome === 'Ana');
+console.log(Ana);
 
 // filtrando objetos
-const usuariosAcimaDe30 = usuarios.filter(usuario => usuario.idade > 30);
+const usuariosAcimaDe30 = usuarios.filter(usuarios => usuarios.idade > 30);
 console.log(usuariosAcimaDe30);
 
 // ordenando obetos por idade
-const usuariosOrdenadosPorIdade = usarios.sort((a,b) => a.idade - b.idade);
+const usuariosOrdenadosPorIdade = usuarios.sort((a,b) => a.idade - b.idade);
 console.log(usuariosOrdenadosPorIdade);
+
+
+// ordenando obetos por idade b.idade - a.idade 
+const usuariosOrdenadosPorIdadev = usuarios.sort((a,b) => b.idade - a.idade);
+console.log(usuariosOrdenadosPorIdade);
+
